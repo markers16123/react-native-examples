2e508ff046dd05858ee270ba98243dcfc428ffd2
markers16123 <markers16123@gmail.com>
2018-07-25 18:13:01 +0900

    fucking-weather

---
+++ fucking-weather/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import Weather from './Weather';
+
+const API_KEY = 'f1beac7143c585ebe289c73ac9fcdd30';
+
+export default class App extends Component {
+  state = {
+    isLoaded: false,
+    error: null,
+    temperature: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: error
+        });
+      }
+    );
+  }
+
+  /**
+   * 날씨 정보를 불러옵니다.
+   */
+  _getWeather = (lat, lon) => {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}`
+    )
+      .then(response => response.json())
+      .then(json =>
+        this.setState({
+          temperature: json.main.temp,
+          name: json.weather[0].main,
+          isLoaded: true
+        })
+      );
+  };
+
+  render() {
+    const { isLoaded, error, temperature, name } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar hidden={true} />
+        {isLoaded ? (
+          <Weather temp={Math.floor(temperature - 273.15)} name={name} />
+        ) : (
+          <View style={styles.loading}>
+            <Text style={styles.loadingText}>Getting the funcking weather</Text>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+        )}
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff'
+  },
+  errorText: {
+    color: 'red',
+    backgroundColor: 'transparent',
+    marginBottom: 40
+  },
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText: {
+    fontSize: 30,
+    marginBottom: 100
+  }
+});
